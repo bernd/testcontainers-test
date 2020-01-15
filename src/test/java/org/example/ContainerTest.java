fb87ad3677f001b3804efc7f4b97a7df8301fa30
Bernd Ahlers <bernd@graylog.com>
2020-01-15 13:50:22 +0100

    Initial commit

---
+++ src/test/java/org/example/ContainerTest.java
@@ -0,0 +1,17 @@
+package org.example;
+
+import org.junit.Rule;
+import org.junit.Test;
+import org.testcontainers.containers.GenericContainer;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class ContainerTest {
+    @Rule
+    public final GenericContainer container = new GenericContainer<>("mongo:4.0");
+
+    @Test
+    public void testContainers() {
+        assertThat(container.getContainerId()).isEqualTo("yolo");
+    }
+}
